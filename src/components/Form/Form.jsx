8d5cf44b61ead7a3ae6b3bdbfca35eb7c6036631
Alex <belohvostov.belohvostov@gmail.com>
2022-10-06 16:08:54 +0300

    corrected import

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import { useCallback } from "react";
+import React, { useEffect, useCallback } from "react";
 
 import { useInput } from "../../hooks/useInput";
 import { useTelegram } from "../../hooks/useTelegram";
